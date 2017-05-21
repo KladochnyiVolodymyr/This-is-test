413c600e5498367defc12a2a4ed8e8b335185cd2
Volodymyr Kladochnyi <kladochnyi.v@gmail.com>
2017-05-21 16:02:02 +0300

    chenges to form

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
 $('.header-content_right').animate({left:'60%', opacity: 1},1500);
 $('.header-content_left').animate({right:'40%', opacity: 1},1500);
 $(document).ready(function() {
-    
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
-    
         /* Check the location of each desired element */
         $('.form').each( function(i){
-            
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                    
-            }
-            
+                $(this).animate({'opacity':'1'},500);       
+            }            
         }); 
-    
-    });
-    
+    }); 
+});
+$( ".form__button").click(function() {
+    var $block = $(".form__button_sending");
+    $block.show();
 });
